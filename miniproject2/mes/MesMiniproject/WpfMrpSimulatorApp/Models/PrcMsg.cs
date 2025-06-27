58a5d0c8a9a2c9b2abaadbbde6dbf228bc00336f
DongKuk-Kim <yum0153@gmail.com>
2025-06-27 12:19:04 +0900

    10일차 커밋

---
+++ miniproject2/mes/MesMiniproject/WpfMrpSimulatorApp/Models/PrcMsg.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfMrpSimulatorApp.Models
+{
+    public class PrcMsg
+    {
+        public string ClientId { get; set; }
+        public string PlantCode { get; set; }
+        public string FacilityId { get; set; }
+        public string Timestamp { get; set; }
+        public string Flag { get; set; }
+
+
+    }
+}
